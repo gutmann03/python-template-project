d3f8d3aecc62dbb98c3bd16f7321139a74fbed50
Volodymyr Smetaniuk <gutmann003@gmail.com>
2024-03-17 00:46:02 +0200

    task 4.2. added tests for read_from_file_pandas function.

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -18,5 +18,21 @@ class TestReadFromFile(unittest.TestCase):
             read_from_file('./data/unknown.txt')
 
 
+class TestReadFromFilePandas(unittest.TestCase):
+
+    def test_read_from_file(self):
+        text_from_file = read_from_file_pandas('./data/jsn.json')
+        print(type(text_from_file))
+        self.assertTrue(len(text_from_file) > 0)
+
+    def test_read_from_file_invalid_type(self):
+        with self.assertRaises(TypeError) as _:
+            read_from_file_pandas(5)
+
+    def test_read_from_non_existing_file(self):
+        with self.assertRaises(FileNotFoundError) as _:
+            read_from_file_pandas('./data/unknown.json')
+
+
 if __name__ == '__main__':
     unittest.main()
