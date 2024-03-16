f9e3fbab070560588230018d1a118df4dfe6f73c
Volodymyr Smetaniuk <gutmann003@gmail.com>
2024-03-16 23:40:23 +0200

    task 4.1. created empty functions for read and write text.

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def read_from_console():
+    pass
+
+
+def read_from_file(file_path: str):
+    pass
+
+
+def read_from_file_pandas(file_path: str):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def write_to_console(text: str):
+    pass
+
+
+def write_to_file(file_path: str, test: str):
+    pass
+
+
+def write_to_file_pandas(file_path: str, test: str):
+    pass
